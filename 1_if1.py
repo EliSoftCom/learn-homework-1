2508e8abac22fc52d0f3dd39ea493f91d2277c8d
Andrei Tsitsilin <tsitsilinav@gmail.com>
2024-05-21 23:07:25 +0300

    The 1st and second exercises are completed

--- 1_if1.py
+++ 1_if1.py
@@ -4,22 +4,39 @@
 
 Условный оператор: Возраст
 
-* Попросить пользователя ввести возраст при помощи input и положить 
+* Попросить пользователя ввести возраст при помощи input и положить
   результат в переменную
-* Написать функцию, которая по возрасту определит, чем должен заниматься пользователь: 
+* Написать функцию, которая по возрасту определит, чем должен заниматься пользователь:
   учиться в детском саду, школе, ВУЗе или работать
-* Вызвать функцию, передав ей возраст пользователя и положить результат 
+* Вызвать функцию, передав ей возраст пользователя и положить результат
   работы функции в переменную
 * Вывести содержимое переменной на экран
 
 """
+flag = True
+question_age = None
+while flag:
+    temp_age = input("Enter your age, please: ")
+    if not (temp_age.isdigit()):
+        print("Input is incorrect")
+    else:
+        question_age = int(temp_age)
+        flag = False
 
-def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+
+def main(age: int) -> str:
+    profession = None
+    if 0 < age < 7:
+        profession = "Attends kindergarten"
+    elif 7 <= age < 18:
+        profession = "Attends school"
+    elif 18 <= age < 23:
+        profession = "Attends institute"
+    else:
+        profession = "Attends dream job"
+        return profession
+
 
 if __name__ == "__main__":
-    main()
+    activity = main(question_age)
+    print(f"Type of activity: {activity}")
