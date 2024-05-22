7a3331405109c03b581f720c0275ced42b9f7849
EliSoftCom <149623823+EliSoftCom@users.noreply.github.com>
2024-05-22 16:59:01 +0300

    Update 3_for.py
    Exercise 3 is completed

--- 3_for.py
+++ 3_for.py
@@ -6,7 +6,7 @@
 
 * Дан список словарей с данными по колличеству проданных телефонов
   [
-    {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]}, 
+    {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]},
     {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
     {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
   ]
@@ -16,12 +16,52 @@
 * Посчитать и вывести среднее количество продаж всех товаров
 """
 
-def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
-    
+
+def forms_in_par(text_title: str):
+    print('*' * len(text_title))
+    print(text_title)
+    print('*' * len(text_title))
+
+
+def main(products: list):
+    forms_in_par("Количество продаж для каждого товара")
+    for dict_temp in products:
+        product = dict_temp['product']
+        sales = dict_temp['items_sold']
+        print(f"Текущий продук - {product}:\nБыл продан общим числом - {sum(sales)}")
+        print()
+
+    forms_in_par("Среднее количество продаж для каждого товара")
+
+    for dict_temp in products:
+        product = dict_temp['product']
+        sales = dict_temp['items_sold']
+        print(f"Средние продажи товара {product} за месяц: {int(sum(sales)/len(sales))}")
+        print()
+
+    forms_in_par("Суммарное количество продаж всех товаров")
+
+    sum_product = 0
+    for dict_temp in products:
+        sales = dict_temp['items_sold']
+        sum_product += sum(sales)
+    print(f"Суммарные продажи - {sum_product} шт.")
+    print()
+
+    forms_in_par("Cреднее количество продаж всех товаров за месяц")
+
+    for i in range(12):
+        sales_month_total = 0
+        for dict_temp in products:
+            sales_month = dict_temp['items_sold'][i]
+            sales_month_total += sales_month
+        print(f"Средние продажи всех товаров за {i+1} месяц: {int(sales_month_total / len(products))}")
+
+
 if __name__ == "__main__":
-    main()
+    products = [
+        {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]},
+        {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
+        {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
+    ]
+    main(products)
